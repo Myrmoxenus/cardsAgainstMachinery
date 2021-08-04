93b24d59cc1bf4d5eb65fb96c5c90d63ae817fb2
Myrmoxenus <scopocira@gmail.com>
2021-08-03 21:28:19 -0500

    Set up websocket correctly, css for cards, test button

--- public/script.js
+++ public/script.js
@@ -1,4 +1,29 @@
-//import {io} from 'socket.io-client'
-import PORT from 'server.js'
-const serverURL = 'http://localhost:' + PORT
-const socket = io.connect(serverURL)
+const serverURL = 'http://localhost:4433';
+const socket = io.connect(serverURL);
+
+let playerHand = document.getElementById('playerHand');
+let testButton = document.getElementById('testButton');
+
+testButton.addEventListener('click', testButtonClick)
+
+function testButtonClick(){
+    socket.emit('testButton', 'testDataWOO');
+}
+
+function createCard(color, content, destination){
+    //Creates card div
+    let cardDiv= document.createElement('div')
+    //Assigns div the card class and the appropriate card color class
+    let cardDivClass = 'card ' + color + 'Card'
+    cardDiv.className = cardDivClass
+    //Creates div for card's conten
+    let contentDiv = document.createElement('div')
+    //Sets appropriate class and innerHTML content for the card content div
+    contentDiv.className = 'cardContent'
+    contentDiv.innerHTML = content
+    //Appends cardContent into card div
+    cardDiv.appendChild(contentDiv)
+    //Appends card to destination
+    destination.appendChild(cardDiv)
+}
+
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 
+//Card Decks
+let whiteCardArray = require('./cards/GeneratedWhiteCardsTest.js')
+let testArray = ['fuck', 'you']
+
 //Sets port to a value
 const PORT = process.env.PORT || 4433;
 
@@ -23,8 +27,20 @@ server.listen(PORT, () => {
     console.log('Server listening on port: ' + PORT);
   });
 
+  function randomUpTo(randomMax) {
+    return ((Math.floor(Math.random() * (randomMax + 1))))
+  }
+  
 //Socket behavior
 io.on('connection', (socket) => {
+  socket.on('testButton', function(data){
+    console.log(whiteCardArray[randomUpTo(whiteCardArray.length-1)])
+  });
     console.log('User connected with socketID: ' + socket.id);
   });
-  
+
+
+console.log(typeof whiteCardArray)
+console.log(typeof testArray)
+console.log(whiteCardArray)
+console.log(testArray[0])
