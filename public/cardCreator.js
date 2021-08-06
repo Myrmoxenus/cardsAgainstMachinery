6d37c829fb5ca65315be8beac0113907f07ff5a6
Myrmoxenus <scopocira@gmail.com>
2021-08-06 00:27:54 -0500

    UI and scaling

---
+++ public/cardCreator.js
@@ -0,0 +1,47 @@
+let testCardContent = document.getElementById('testCardContent');
+
+//Function that tests how many lines card content will take up.
+function numberOfLines(cardContent){
+    //Determines the height of a single line based on current view window by filling in an invisible card and checking
+    testCardContent.innerHTML = 'testLine'
+    let singleLineHeight = document.getElementById('testCardContent').clientHeight
+    //Determines the number of lines of card content by checking total height and dividing it by the height of a single line and rounding it to the nearest integer
+    testCardContent.innerHTML = cardContent
+    let numberOfLines =  Math.round(document.getElementById('testCardContent').clientHeight/singleLineHeight)
+    //Resets inner HTML of hidden card content
+    testCardContent.innerHTML = ''
+    return numberOfLines
+}
+
+//Function that appends a card at a supplied location with supplied content with formatting
+function createCard(destination, cardContent, color){
+    cardContent = cardContent || ' '
+    color = color || 'white'
+    //Creates card div
+    let cardDiv= document.createElement('div')
+    //Assigns div the card class and the appropriate card color class
+    let cardDivClass = 'card ' + color + 'Card'
+    cardDiv.className = cardDivClass
+    //Determines the number of lines of the card content
+    let contentHeight = numberOfLines(cardContent)
+    let contentDivClass = 'cardContent'
+     if(contentHeight === 1){
+        contentDivClass += ' shortContent'
+     }
+    //Creates div for card's content
+    let contentDiv = document.createElement('div')
+    //Sets appropriate class and innerHTML content for the card content div
+    contentDiv.className = contentDivClass
+    contentDiv.innerHTML = cardContent
+    //Attempts to center content based on length of card content, 4.9 and 0.75 are magic numbers.
+    let dynamicPadding = 0
+    if(contentHeight < 7){
+        dynamicPadding = 4.9 - contentHeight*0.75
+    }
+    contentDiv.style.paddingTop = dynamicPadding + 'vw'
+    //Appends cardContent into card div
+    cardDiv.appendChild(contentDiv)
+    //Appends card to destination
+    destination.appendChild(cardDiv)
+}
+
--- public/script.js
+++ public/script.js
@@ -4,8 +4,41 @@ const serverURL = 'http://localhost:4433';
 const socket = io.connect(serverURL);
 
 let playerHand = document.getElementById('playerHand');
+let playTable = document.getElementById('playTable');
 let testButton = document.getElementById('testButton');
-let testCardContent = document.getElementById('testCardContent');
+let informationPanel = document.getElementById('informationPanel');
+
+function createNamePlates(nameArray){
+    
+    function createNamePlate(name){
+        let namePlateDiv = document.createElement('div')
+        namePlateDiv.className = 'namePlate'
+        namePlateDiv.style.height = 90/nameArray.length + '%'
+        let namePlateContentDiv = document.createElement('div')
+        namePlateContentDiv.className = 'namePlateContent'
+        let namePlateFontSize = 2
+        
+        if(name.length > 63){
+            let extraTextLength = name.length - 63
+            namePlateFontSize = 2 - extraTextLength*0.0022
+            if (namePlateFontSize < 0.2){
+                namePlateFontSize = 0.2
+            }
+        }
+        if(name.length>15700){
+            namePlateDiv.style.overflowY='scroll'
+        }
+        namePlateContentDiv.style.fontSize = namePlateFontSize + 'vw'
+        namePlateContentDiv.innerHTML = name
+        
+        informationPanel.appendChild(namePlateDiv)
+        namePlateDiv.appendChild(namePlateContentDiv)
+
+    }
+
+    nameArray.forEach(name => createNamePlate(name))
+
+}
 
 testButton.addEventListener('click', testButtonClick);
 
@@ -13,50 +46,13 @@ function testButtonClick(){
     socket.emit('testButton');
 }
 
-function numberOfLines(cardContent){
-    //Determines the height of a single line based on current view window by filling in an invisible card and checking
-    testCardContent.innerHTML = 'testLine'
-    let singleLineHeight = document.getElementById('testCardContent').clientHeight
-    //Determines the number of lines of card content by checking total height and dividing it by the height of a single line and rounding it to the nearest integer
-    testCardContent.innerHTML = cardContent
-    let numberOfLines =  Math.round(document.getElementById('testCardContent').clientHeight/singleLineHeight)
-    //Resets inner HTML of hidden card content
-    testCardContent.innerHTML = ''
-    return numberOfLines
-}
-
-function createCard(destination, cardContent, color){
-    color = color || 'white'
-    //Creates card div
-    let cardDiv= document.createElement('div')
-    //Assigns div the card class and the appropriate card color class
-    let cardDivClass = 'card ' + color + 'Card'
-    cardDiv.className = cardDivClass
-    //Determines the number of lines of the card content
-    let contentHeight = numberOfLines(cardContent)
-    let contentDivClass = 'cardContent'
-     if(contentHeight === 1){
-        contentDivClass += ' shortContent'
-     }
-    //Creates div for card's content
-    let contentDiv = document.createElement('div')
-    //Sets appropriate class and innerHTML content for the card content div
-    contentDiv.className = contentDivClass
-    contentDiv.innerHTML = cardContent
-    //Attempts to center content based on length of card content, 4.9 and 0.7 are magic numbers.
-    let dynamicPadding = 0
-    if(contentHeight < 7){
-        dynamicPadding = 4.9 - contentHeight*0.7
-    }
-    contentDiv.style.paddingTop = dynamicPadding + 'vw'
-    //Appends cardContent into card div
-    cardDiv.appendChild(contentDiv)
-    //Appends card to destination
-    destination.appendChild(cardDiv)
-}
-
 // Listen for events
 socket.on('drawWhiteCard', function(data){
     createCard(playerHand, data)
 });
 
+createNamePlates(['Zack', 
+'Fuck you', 
+'Fuck you FUck you fuck you fuck you fuck you fuck youyou fuck you fuck youyou fuck you fuck yfuck you fuFuck you FUck you fuck you fuck you fuck you fuck youyou fuck you fuck youyou fuck you fuck yfuck you fuck you fuck you fuck youFuck you FUck you fuck you fuck you fuck you fuck youyou fuck you fuck youyou fuck you fuck yfuck you fuck you fuck you fuck youFuck you FUck you fuck you fuck you fuck you fuck youyou fuck you fuck youyou fuck you fuck yfuck you fuck you fuck you fuck youFuck you FUck you fuck you fuck you fuck you fuck youyou fuck you fuck youyou fuck you fuck yfuck you fuck you fuck you fuck youck you fuck you END',
+ 'Fuck you',
+ 'Horse. Horse Horse Horse Horse Horse Horse Horse horse WEWERERE RERERE fuck fuck fuck'])
