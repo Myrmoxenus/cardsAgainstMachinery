b4d25b6d267db744788f73534adc928b2eb03651
Myrmoxenus <scopocira@gmail.com>
2021-10-08 19:33:57 -0500

    Uploaded cards

---
+++ cards/GeneratedWhiteCardsTest.js
@@ -0,0 +1,31 @@
+let generatedWhiteCardString = `A blowjob from Jesus.
+Finger-banging a god damned statue.
+A crazy little horse.
+The internet of things.
+A pinky-slapping idiot.
+A happy lady with thousands of boyfriends.
+A baby with a majorly boring penis.
+The perils of public nudity.
+Mere mortals.
+The driver.
+The best man.
+One too many beers.
+Bye bye, mom.
+The world's greatest conman.
+A madcap waffle made from a male breast.
+A cross-racial handshake.
+A disheartening amount of makeup.
+The smell of death.
+A very rich man.
+A little bit of alcohol.
+The world's biggest homeless person.
+The ability to make a donation to the local orphanage.
+A traditional, church-approved gay wedding.
+No pants.
+A little bit of pain.
+The ability to talk.
+A dead baby.
+Eight 18-year-olds who don't know when to stop
+`
+let generatedWhiteCardArray = generatedWhiteCardString.split('\n')
+module.exports = generatedWhiteCardArray ;
